e39f564cda00a39c23ffbec90f2916c50ea8764d
GiaHuy999 <huyphan2792005@gmail.com>
2025-09-14 21:45:23 +0700

    feat: initialize Flask project

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+from models import db
+
+app = Flask(__name__)
+
+# Kết nối MySQL
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:123456@localhost/huutinhdoor"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
